320694b8902bf588ede9820098c09bcb06e22a89
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-14 07:34:53 +0530

    Create madmax.py

---
+++ SYSTUM/madmax.py
@@ -0,0 +1,20 @@
+STICKER = [
+    "CAACAgUAAxkBAAIf1mXLhNqRe8r0L4BWx1KoEH4pczLWAAIzBQACUFMRV8RRoNnVRpmaHgQ",
+    "CAACAgUAAxkBAAIf1WXLhNcP5rJlcVbuC9SRLMobCqy7AAJlCAACAqsQV276fswAAZn_Eh4E",
+    "CAACAgUAAxkBAAIf1GXLhMppz0sc9ZjjEVtODxEQtDCUAALwBgAC3scQV4N0U4MeYX_rHgQ",
+    "CAACAgUAAxkBAAIf02XLhMYRKBafDi6tvZg154R7xRqCAAI2BgACSF4YV3PG1tY8zOd5HgQ",
+]
+
+
+EMOJIOS = [
+    "💣",
+    "💥",
+    "🪄",
+    "🧨",
+    "⚡",
+    "🤡",
+    "👻",
+    "🎃",
+    "🤤",
+    "💞",
+]
