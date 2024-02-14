8d0995c849e95f359bf28626ca57b4509d96a3f6
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-14 07:19:35 +0530

    Update start.py

--- SYSTUM/plugins/bot/start.py
+++ SYSTUM/plugins/bot/start.py
@@ -1,14 +1,17 @@
 import time
-
+import random 
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
-
+import asyncio
 import config
 from SYSTUM import app
 from SYSTUM.misc import _boot_
 from SYSTUM.plugins.sudo.sudoers import sudoers_list
+from SYSTUM.madmax import EMOJIOS, STICKER
+from SYSTUM.utils.database import get_served_chats, get_served_users, get_sudoers
+from SYSTUM.utils import bot_sys_stats
 from SYSTUM.utils.database import (
     add_served_chat,
     add_served_user,
@@ -24,6 +27,29 @@ from config import BANNED_USERS
 from strings import get_string
 
 
+
+YUMI_PICS = [
+"https://telegra.ph/file/23f73435a6ecfd4672f7a.jpg",
+"https://telegra.ph/file/876d368cdbc7ff8ec9da1.jpg",
+"https://telegra.ph/file/e612d72e6c34169d25f0a.jpg",
+"https://telegra.ph/file/bcd8260e9a0b017c6093a.jpg",
+"https://telegra.ph/file/f34821eda0b7f6d6dd1e0.jpg",
+"https://telegra.ph/file/d2de9d8ae78ea6728a396.jpg",
+"https://telegra.ph/file/98f4a927d3ece2d533288.jpg",
+"https://telegra.ph/file/3a0193ab0784f4ea72e01.jpg",
+"https://telegra.ph/file/908acc43a5bc5efa3eb64.jpg",
+"https://telegra.ph/file/4da8c3c33986ddcf6b3bf.jpg",
+"https://telegra.ph/file/08d0607336afdd7cb657e.jpg",
+"https://telegra.ph/file/a3c08fe02de2e0a44cc38.jpg",
+"https://telegra.ph/file/faab37992164e1a4302d7.jpg",
+"https://telegra.ph/file/48311cb6c7f2c81044181.jpg",
+"https://telegra.ph/file/3f6feb47b97b7ae433e64.jpg",
+"https://telegra.ph/file/498319eff274c25b1ddbe.jpg",
+"https://telegra.ph/file/bf4bfec900f50613f3659.jpg",
+"https://telegra.ph/file/9f21e49f1f3c4e10f29d7.jpg",
+  ]
+
+
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
@@ -32,10 +58,25 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
+            accha = await message.reply_text(
+            text=random.choice(EMOJIOS),
+            )
+            await asyncio.sleep(1.3)
+            await accha.edit("ᴅιиg ᴅσиg ꨄ︎ ѕтαятιиg.....")
+            await asyncio.sleep(0.2)
+            await accha.edit("ᴅιиg ᴅσиg ꨄ sтαятιиg.........")
+            await asyncio.sleep(0.2)
+            await accha.edit("ᴅιиg ᴅσиg ꨄ︎ sтαятιиg.....")
+            await asyncio.sleep(0.2)
+            await accha.delete()
+            umm = await message.reply_sticker(sticker=random.choice(STICKER))
+            await asyncio.sleep(2)
+            await umm.delete()
             return await message.reply_photo(
-                photo=config.START_IMG_URL,
+                random.choice(YUMI_PICS),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
+                has_spoiler=True,
             )
         if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
@@ -45,6 +86,16 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
             return
+        if name == "verify":
+            await message.reply_text(f"ʜᴇʏ {message.from_user.first_name},\nᴛʜᴀɴᴋs ғᴏʀ ᴠᴇʀɪғʏɪɴɢ ʏᴏᴜʀsᴇʟғ ɪɴ ˹ʟ ᴠ ʏ ☔ ᴍ ᴜ s ɪ ᴄ 🎶˼, ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ɢᴏ ʙᴀᴄᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴜsɪɴɢ ᴍᴇ.")
+            if await is_on_off(2):
+                sender_id = message.from_user.id
+                sender_name = message.from_user.first_name
+                return await bot.send_message(
+                    config.LOG_GROUP_ID,
+                    f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ <code>ᴠᴇʀɪғʏ ʜɪᴍsᴇʟғ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
+                )
+            return    
         if name[0:3] == "inf":
             m = await message.reply_text("🔎")
             query = (str(name)).replace("info_", "", 1)
@@ -84,10 +135,28 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
+        served_chats = len(await get_served_chats())
+        served_users = len(await get_served_users())
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+        accha = await message.reply_text(
+            text=random.choice(EMOJIOS),
+        )
+        await asyncio.sleep(1.3)
+        await accha.edit("ᴅιиg ᴅσиg ꨄ︎ ѕтαятιиg........")
+        await asyncio.sleep(0.2)
+        await accha.edit("ᴅιиg ᴅσиg ꨄ sтαятιиg........")
+        await asyncio.sleep(0.2)
+        await accha.edit("ᴅιиg ᴅσиg ꨄ︎ sтαятιиg......")
+        await asyncio.sleep(0.2)
+        await accha.delete()
+        umm = await message.reply_sticker(sticker=random.choice(STICKER))
+        await asyncio.sleep(2)
+        await umm.delete()
         await message.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            random.choice(YUMI_PICS),
+            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
             reply_markup=InlineKeyboardMarkup(out),
+            has_spoiler=True,
         )
         if await is_on_off(2):
             return await app.send_message(
@@ -102,7 +171,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        photo=config.START_IMG_URL,
+        random.choice(YUMI_PICS),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -137,9 +206,9 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    random.choice(YUMI_PICS),
                     caption=_["start_3"].format(
-                        message.from_user.first_name,
+                        message.from_user.mention,
                         app.mention,
                         message.chat.title,
                         app.mention,
